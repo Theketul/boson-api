16e77e17155871136ed18a1c7e9dc5248148cc6b
ketulp74 <ketul@missionsustainability.org>
2025-06-22 20:55:52 +0530

    allowed the cors

--- middleware.js
+++ middleware.js
@@ -6,8 +6,32 @@ const ResponseHandler = require('./config/responseHandler');
 const setupMiddleware = (app) => {
     app.use(bodyParser.json({ limit: '1000mb' }));
     app.use(bodyParser.urlencoded({ limit: '1000mb', extended: true }));
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(cors());
+
+    const allowedOrigins = [
+        'https://boson-app.vercel.app',     // staging
+        'https://ops.waters.co.in'          // production
+    ];
+
+    const corsOptions = {
+        origin: function (origin, callback) {
+            if (
+                !origin || // allow non-browser tools like Postman
+                allowedOrigins.includes(origin) || // known frontends
+                /^http:\/\/localhost(:\d+)?$/.test(origin) // any localhost:<port>
+            ) {
+                callback(null, true);
+            } else {
+                console.warn(`CORS blocked origin: ${origin}`);
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true
+    };
+
+    app.use(cors(corsOptions));
+    app.options('*', cors(corsOptions)); // preflight handling
     app.use(express.json());
     app.use((req, res, next) => {
         res.handler = new ResponseHandler(req, res);
